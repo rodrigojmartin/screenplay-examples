b41501d1f25328b33eaee3706996171fb1441d54
Rodrigo Martin <rodrigo.martin@avature.net>
2017-07-30 19:38:28 -0300

    Refactor page object to use target and be screenplay oriented.

--- src/test/java/com/testingar/screenplay/pages/HomePage.java
+++ src/test/java/com/testingar/screenplay/pages/HomePage.java
@@ -1,20 +1,13 @@
 package com.testingar.screenplay.pages;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebElement;
 
 @DefaultUrl("https://www.amazon.com/")
 public class HomePage extends PageObject {
 
-    @FindBy(css = "input[value='Go']")
-    WebElement searchButton;
+    public static Target SEARCH_FIELD =  Target.the("search field").locatedBy("#twotabsearchtextbox");
 
-    @FindBy(css = "#twotabsearchtextbox")
-    WebElement searchQuery;
-
-    public void searchFor(String keywords) {
-        searchQuery.sendKeys(keywords);
-        searchButton.click();
-    }
 }
