7f558421794a69187afce03abdfd14255b4075ab
Rodrigo Martin <rodrigo.martin@avature.net>
2017-09-03 23:02:33 -0300

    Regexp for matching Step Definitions + Question validation

--- src/test/java/com/testingar/screenplay/steps/SearchByKeywordStepsDefinition.java
+++ src/test/java/com/testingar/screenplay/steps/SearchByKeywordStepsDefinition.java
@@ -1,5 +1,6 @@
 package com.testingar.screenplay.steps;
 import com.testingar.screenplay.model.Section;
+import com.testingar.screenplay.questions.TheResults;
 import com.testingar.screenplay.steps.serenity.BuyerSteps;
 import com.testingar.screenplay.tasks.Navigate;
 import com.testingar.screenplay.tasks.Search;
@@ -7,13 +8,17 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.thucydides.core.annotations.Steps;
+import org.hamcrest.Matcher;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertTrue;
 
 
@@ -28,22 +33,23 @@ public class SearchByKeywordStepsDefinition {
     BuyerSteps buyer;
 
     @Given("^that (.*) wants to buy cool stuff in Amazon$")
-    public void richard_wants_to_buy_cool_stuff_in_Amazon(String personaName) throws Throwable {
+    public void wants_to_buy_cool_stuff_in_Amazon(String personaName) throws Throwable {
         theActorCalled(personaName).attemptsTo(
                 Navigate.to(Section.Amazon)
         );
     }
 
-    @When("^he search for items containing (.*)$")
-    public void he_search_for_items_containing(String keywords) throws Throwable {
+    @When("^(?:he|she) searches for items containing (.*)$")
+    public void searches_for_items_containing(String keywords) throws Throwable {
         theActorInTheSpotlight().attemptsTo(
                 Search.byKeywords(keywords)
         );
     }
 
-    @Then("^he should only see items related to (.*)$")
-    public void he_should_only_see_items_related(String keywords) throws Throwable {
-        assertTrue(buyer.should_only_see_items_related_to(keywords));
+    @Then("^(?:he|she) should only see items related to (.*)$")
+    public void should_only_see_items_related(String keywords) throws Throwable {
+        theActorInTheSpotlight().should(seeThat(TheResults.displayed(), containsString(keywords)));
     }
+
 
 }
