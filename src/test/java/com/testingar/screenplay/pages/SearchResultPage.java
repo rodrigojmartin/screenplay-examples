e547e11b3641f18b380cb2059af419f447141ada
Rodrigo Martin <rodrigo.martin@avature.net>
2017-09-03 23:01:33 -0300

    PageObject classes should only have TARGET locators

--- src/test/java/com/testingar/screenplay/pages/SearchResultPage.java
+++ src/test/java/com/testingar/screenplay/pages/SearchResultPage.java
@@ -2,18 +2,12 @@ package com.testingar.screenplay.pages;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class SearchResultPage extends PageObject {
 
-    @FindBy(css = "h2#s-result-count")
-    WebElement resultsCount;
+    public static Target RESULT_BREADCRUMB =  Target.the("result breadcrumb").located(By.cssSelector("div#searchTemplate"));
 
-    public boolean resultShouldContain(String keyword) {
-        if (resultsCount.getText().contains(keyword)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
